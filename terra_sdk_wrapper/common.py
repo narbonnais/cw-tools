5229c034974640303e15cdb5c4111ababb78d39f
Lua <alexisfabre.dev@gmail.com>
2022-01-31 10:35:00 +0100

    Typo fix

--- terra_sdk_wrapper/common.py
+++ terra_sdk_wrapper/common.py
@@ -84,7 +84,7 @@ def execute_contract(terra: LCDClient, sender: Wallet, contract_address: str, ex
         tx = sender.create_and_sign_tx(
             msgs=[execute])
         result = terra.tx.broadcast(tx)
-        print(chalk.green(f"[+] Success executiing {execute_msg}"))
+        print(chalk.green(f"[+] Success executing {execute_msg}"))
         return result
     except Exception as e:
         print(chalk.red(f"[!] Error executing {execute_msg}"))
